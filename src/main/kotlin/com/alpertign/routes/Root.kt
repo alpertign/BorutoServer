864402cc0eaab9d608e0d51f67f4b6f6a41dadb1
Dilara Acikgoz <dilaraaa.acar@gmail.com>
2023-01-03 01:06:21 +0300

    - routes/Root added - bug fixed in application.conf

---
+++ src/main/kotlin/com/alpertign/routes/Root.kt
@@ -0,0 +1,17 @@
+package com.alpertign.routes
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.get
+
+
+fun Route.root(){
+    get("/"){
+        call.respond(
+            message = "Welcome to Boruto API!",
+            status = HttpStatusCode.OK
+        )
+    }
+}
